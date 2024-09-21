ebf92e280ae0b03d3f79b18e261101fbfdbcc232
showrov4g <showrovg00@gmail.com>
2024-09-21 22:01:55 +0600

    function setup

--- js/function.js
+++ js/function.js
@@ -1,11 +1,18 @@
 function inputFieldWithId(id){
-    let inputValue =  document.getElementById(id).value;
-    return inputValue;
+    // let inputValue =  document.getElementById(id).value;
+    // return inputValue;
+    return parseFloat(document.getElementById(id).value);
 }
 
 function showSection(id){
-    let show = document.getElementById(id).classList.remove('hidden')
-    return show;
+    // let show = document.getElementById(id).classList.remove('hidden')
+    // return show;
+    return parseFloat(document.getElementById(id).classList.remove('hidden'));
+}
+
+function hideSection(id){
+    let hide = document.getElementById(id).classList.add('hidden');
+    return hide;
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,26 +1,29 @@
+document.getElementById("assistant-tab").addEventListener("click", function () {
+  console.log("button click");
+});
+
 document
-  .getElementById("assistant-tab")
-  .addEventListener("click", function () {
-    console.log("button click");
+  .getElementById("calculate")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let income = inputFieldWithId("income");
+    let software = inputFieldWithId("software");
+    let course = inputFieldWithId("courses");
+    let internet = inputFieldWithId("internet");
+
+    // section show
+    showSection("results");
   });
 
-
-  document.getElementById('calculate').addEventListener('click',function(event){
-    event.preventDefault();
-    let income = inputFieldWithId('income');
-    let software = inputFieldWithId('software');
-    let course = inputFieldWithId('courses');
-    let internet = inputFieldWithId('internet')
-
-    // section show     
-    showSection('results');
-
-
-  })
-
-
-
-
-//   show hide section 
-
-
+document.getElementById("income").addEventListener("input", function () {
+  let income = inputFieldWithId("income");
+  let software = inputFieldWithId("software");
+  let course = inputFieldWithId("courses");
+  let internet = inputFieldWithId("internet");
+  if (income <= 0 || isNaN(income)) {
+    showSection("income-error");
+  } else {
+    hideSection("income-error");
+  }
+});
